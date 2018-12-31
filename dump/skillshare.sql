061bb0f4844600e12f7ecd0b5d1f91ce0b2008b0
pjayneet97 <porwaljayneet97@gmail.com>
2018-12-31 22:48:51 +0530

    updated database broken user_name into user_fname and user_lname

--- dump/skillshare.sql
+++ dump/skillshare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 31, 2018 at 08:19 AM
+-- Generation Time: Dec 31, 2018 at 06:18 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -42,8 +42,7 @@ CREATE TABLE `graduation` (
 --
 
 INSERT INTO `graduation` (`graduation_id`, `user_id`, `college_name`, `degree_name`, `subject`, `year_of_completion`) VALUES
-(1, 1, 'college of technology and engineering', 'b.tech', 'computer science', '2020'),
-(2, 2, 'pacific univercity', 'b.tech', 'mechanical', '2021');
+(5, 5, 'ctae', 'b.tech', 'computer science', '2020');
 
 -- --------------------------------------------------------
 
@@ -64,8 +63,7 @@ CREATE TABLE `school` (
 --
 
 INSERT INTO `school` (`school_id`, `user_id`, `school_name`, `school_board`, `school_percentage`) VALUES
-(1, 1, 'st. gregorios senior sec. school', 'CBSE', '93.4'),
-(2, 2, 'central academy', 'RBSE', '83');
+(5, 5, 'st gregorios senior sec school', 'cbse', '93.4');
 
 -- --------------------------------------------------------
 
@@ -85,10 +83,8 @@ CREATE TABLE `skills` (
 --
 
 INSERT INTO `skills` (`skill_id`, `user_id`, `skill_name`, `skill_level`) VALUES
-(1, 1, 'PHP', 'BEGINNER'),
-(2, 1, 'JAVA', 'ADVANCE'),
-(3, 2, 'HTML', 'INTERMEDIATE'),
-(4, 2, 'DIGITAL MARKETING', 'BEGINNER');
+(20, 5, 'PHP', 'advance'),
+(21, 5, 'ANGULAR6', 'INTERNEDIATE');
 
 -- --------------------------------------------------------
 
@@ -98,11 +94,12 @@ INSERT INTO `skills` (`skill_id`, `user_id`, `skill_name`, `skill_level`) VALUES
 
 CREATE TABLE `user` (
   `user_id` int(10) NOT NULL,
-  `user_name` varchar(255) NOT NULL,
+  `user_fname` varchar(255) NOT NULL,
+  `user_lname` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_phone` varchar(11) NOT NULL,
   `user_password` varchar(25) NOT NULL,
-  `user_profileimg` varchar(255) NOT NULL,
+  `user_profileimg` varchar(255) NOT NULL DEFAULT 'avatar.png',
   `user_city` varchar(20) NOT NULL,
   `experience` int(5) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -111,9 +108,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `user_name`, `user_email`, `user_phone`, `user_password`, `user_profileimg`, `user_city`, `experience`) VALUES
-(1, 'jayneet porwal', 'porwaljayneet97@gmail.com', '8955018041', 'open@123', 'avatar.png', 'udaipur', 0),
-(2, 'lucky soni', 'luckysoni291298@gmail.com', '8875225170', 'open@456', 'avatar.png', 'khamnor', 2);
+INSERT INTO `user` (`user_id`, `user_fname`, `user_lname`, `user_email`, `user_phone`, `user_password`, `user_profileimg`, `user_city`, `experience`) VALUES
+(5, 'jayneet', 'porwal', 'porwaljayneet97@gmail.com', '8955018041', 'open@123', 'avatar.png', 'udaipur', 0);
 
 -- --------------------------------------------------------
 
@@ -135,8 +131,7 @@ CREATE TABLE `work_experience` (
 --
 
 INSERT INTO `work_experience` (`experience_id`, `user_id`, `organisation`, `position`, `duration`, `description`) VALUES
-(1, 1, 'Raj Culture Tours and travel', 'web Developer', '6', 'I managed their Website and marketing'),
-(2, 2, 'orurentals', 'degital marketing head', '3', 'www.orurentals.com');
+(22, 5, 'orurentals', 'web developer', '6 months', 'i creaated the website www.orurentals.com');
 
 --
 -- Indexes for dumped tables
@@ -180,31 +175,31 @@ ALTER TABLE `work_experience`
 -- AUTO_INCREMENT for table `graduation`
 --
 ALTER TABLE `graduation`
-  MODIFY `graduation_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `graduation_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `school`
 --
 ALTER TABLE `school`
-  MODIFY `school_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `school_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `skills`
 --
 ALTER TABLE `skills`
-  MODIFY `skill_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `skill_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `work_experience`
 --
 ALTER TABLE `work_experience`
-  MODIFY `experience_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `experience_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
