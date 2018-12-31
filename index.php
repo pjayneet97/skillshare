6568f5205e73b17dbf15ab214a1660d05f0c3afd
pjayneet97 <porwaljayneet97@gmail.com>
2018-12-31 12:17:51 +0530

    added sorting

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
+<?php 
+    include "includes/functions.php";
+    if(isset($_GET['key'])){
+        $href="index.php?key=".$_GET['key']."&orderbyexp=";
+    }
+    else{
+        $href="index.php?orderbyexp=";
+    }
+?>
 <form action="index.php" method="get">
-    <input type="text" name="key">
+    <input type="text" name="key" required>
     <button type="submit">search</button>
 </form>
-<form action="index.php">
-    <select name="orderbyexp">
-        <option value="none" selected>no sorting</option>
-        <option value="desc">more to less</option>
-        <option value="asc">less to more</option>
-    </select>
-    <button type="submit">sort</button>
-</form>
+        <a href="<?php echo $href.'desc'; ?>">desc</a>
+        <a href="<?php echo $href.'asc'; ?>">asc</a>
+
+      
+
 <?php
-    include "includes/functions.php";
     if($_GET){
         if(isset($_GET['key']) && isset($_GET['orderbyexp'])){
                 $key=$_GET['key'];
